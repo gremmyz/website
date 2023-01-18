b1135f4dde7ea3f792adb0e7735b9c2231e2dd00
Ng Jia Yi <92777033+gremmyz@users.noreply.github.com>
2023-01-18 17:01:17 +0800

    update index.html, styles.scss

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
-const sections = document.querySelectorAll('.section');
-const sectionButtonParent = document.querySelectorAll('.controls');
-const sectionButton = document.querySelectorAll('.control');
-const allSections = document.querySelector('.main-content');
-
-function PageTransition() {
-    // Button click, active class
-    for(let i = 0; i < sectionButton.length; i++) {
-        sectionButton[i].addEventListener('click', function(){
-            let currentButton = document.querySelectorAll('.active-btn');
-            currentButton[0].className = currentButton[0].className.replace(' active-btn', '');
-            this.className += ' active-btn';
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
         })
-    }
-
-    allSections.addEventListener('click', (e)=>{
-        const id = e.target.dataset.id;
-        if (id) {
-            sections.forEach((section) => {
-                section.classList.remove('active');
-            })
-
-            const element = document.getElementById(id);
-            element.classList.add('active');
-        }
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
     })
-}
-
-PageTransition();
+})();
